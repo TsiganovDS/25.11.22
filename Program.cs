0c13763375090b0c30fa061b7d3e166322e7c0ff
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-25 21:46:09 +0700

    25

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿  Console.Write("Введите число A: ");
+  int A = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Введите число B: ");
+  int B = Convert.ToInt32(Console.ReadLine());
+int Exp(int A, int B)
+{
+  int result = 1;
+  for(int i=1; i <= B; i++)
+  {
+    result = result * A;
+  }
+    return result;
+}
+  int exp = Exp(A, B);
+  Console.WriteLine("Результат: " + exp);
