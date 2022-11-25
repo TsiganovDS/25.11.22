12d5212c37f1a10cd2303742a7b8722e83aa4c37
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-25 22:36:05 +0700

    27

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+  int SumNumber(int N)
+  {
+    int counter = Convert.ToString(N).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+      advance = N - N % 10;
+      result = result + (N - advance);
+      N = N / 10;
+    }
+   return result;
+  }
+
+int sumNumber = SumNumber(N);
+Console.WriteLine("Сумма: " + sumNumber);
+
