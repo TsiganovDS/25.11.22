2a62c267b4da276c098000252d70382f6106571c
Даниил Полянский <dm.tsiganov@gmail.com>
2022-11-26 00:07:54 +0700

    29

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int [] numbers = new int[8];
+Console.Write("[");
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 9);
+    Console.Write(numbers[i]);
+    if (i < numbers.Length - 1)
+    {
+        Console.Write(", ");
+    }    
+ }
+Console.Write("]");
+
